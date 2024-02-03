42a152dff2a03b75e64e07ab543dc2eb80c3eecc
Saige Cross <saigecrossdev@gmail.com>
2024-02-03 16:06:06 -0600

    added trips, new trips, getting trips

--- src/app/trips/new/page.tsx
+++ src/app/trips/new/page.tsx
@@ -1,9 +1,114 @@
 "use client"
 
-import React from "react"
+import React, { useState, FormEvent } from "react"
 
 function NewTrip() {
-  return <div>Add a new trip</div>
+  const [title, setTitle] = useState("")
+  const [location, setLocation] = useState("")
+  const [startDate, setStartDate] = useState("")
+  const [endDate, setEndDate] = useState("")
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const res = await fetch("http://localhost:3001/trips", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        trip: {
+          title: title,
+          location: location,
+          start_date: startDate,
+          end_date: endDate,
+        },
+      }),
+      credentials: "include",
+    })
+
+    if (res.ok) {
+      console.log("Trip planned successfully")
+    } else {
+      console.error("Failed to plan trip")
+    }
+
+    setLocation("")
+    setStartDate("")
+    setEndDate("")
+  }
+
+  return (
+    <form className="space-y-4 px-20 py-12" onSubmit={handleSubmit}>
+      <div>
+        <label
+          htmlFor="title"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="location"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Location
+        </label>
+        <input
+          type="text"
+          id="location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="date"
+          className="block text-sm font-medium text-gray-700"
+        >
+          Start Date
+        </label>
+        <input
+          type="date"
+          id="date"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <div>
+        <label
+          htmlFor="date"
+          className="block text-sm font-medium text-gray-700"
+        >
+          End Date
+        </label>
+        <input
+          type="date"
+          id="date"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          required
+        />
+      </div>
+      <button
+        type="submit"
+        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+        Plan Trip
+      </button>
+    </form>
+  )
 }
 
 export default NewTrip
--- src/app/trips/page.tsx
+++ src/app/trips/page.tsx
@@ -1,15 +1,55 @@
 "use client"
 
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useRouter } from "next/navigation"
 
+type Trip = {
+  id: number
+  title: string
+  location: string
+  start_date: string
+  end_date: string
+}
+
 function Trips() {
+  const [trips, setTrips] = useState([])
   const router = useRouter()
+
+  async function fetchTrips() {
+    const res = await fetch("http://localhost:3001/trips", {
+      credentials: "include",
+    })
+
+    if (
+      res.ok &&
+      res.headers.get("Content-Type")?.includes("application/json")
+    ) {
+      const data = await res.json()
+      setTrips(data)
+    } else {
+      console.error("Failed to fetch trips or the response is not JSON")
+    }
+  }
+
+  useEffect(() => {
+    fetchTrips()
+  }, [])
+
   return (
     <div>
       <h1>My Trips</h1>
-      <p>Looks like you have no trips planned!</p>
-      <p>Let&apos;s add a new one.</p>
+      {trips.length > 0 ? (
+        <ul>
+          {trips.map((trip: Trip) => (
+            <li key={trip.id}>
+              {trip.title} - {trip.location} from {trip.start_date} to{" "}
+              {trip.end_date}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>Looks like you have no trips planned!</p>
+      )}
       <button
         className="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
         onClick={() => router.push("/trips/new")}
