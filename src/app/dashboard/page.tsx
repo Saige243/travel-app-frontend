122edfb8a553227be419d3a7c3a2ad6b86e49081
Saige Cross <saigecrossdev@gmail.com>
2024-02-04 13:29:14 -0600

    fetching user on dash

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,43 @@
-import React from "react"
+"use client"
+
+import React, { useState, useEffect } from "react"
+import { useRouter } from "next/navigation"
+
+type User = {
+  id: number
+  first_name: string
+  last_name: string
+  email: string
+}
 
 function Dashboard() {
-  return <div>Dashboard</div>
+  const [user, setUser] = useState<User | null>(null)
+  const router = useRouter()
+
+  const fetchUserData = async () => {
+    const response = await fetch("http://localhost:3001/profile", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    })
+
+    if (response.ok) {
+      const data = await response.json()
+      setUser(data)
+    } else {
+      console.error("Failed to fetch user data", response)
+    }
+  }
+
+  useEffect(() => {
+    fetchUserData()
+  }, [])
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>Welcome to the trip dashboard, {user?.first_name}!</p>
+    </div>
+  )
 }
 
 export default Dashboard
