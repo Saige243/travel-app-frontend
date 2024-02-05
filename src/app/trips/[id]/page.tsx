120ac09a3a947854d60b7cef1b4156ac4e6d69cb
Saige Cross <saigecrossdev@gmail.com>
2024-02-05 12:33:26 -0600

    edit finished

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -1,30 +1,64 @@
 "use client"
 
 import React, { useEffect } from "react"
-import { useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import { useTripData } from "../../_hooks/useTripData"
+import { format, parseISO } from "date-fns"
 
 function TripID() {
   const { id } = useParams()
+  const router = useRouter()
   const { trip, fetchTrip } = useTripData()
 
   useEffect(() => {
     fetchTrip(id as string)
   }, [])
 
+  function formatDate(dateString: string) {
+    const date = parseISO(dateString)
+    return format(date, "EEEE, MMMM d, yyyy")
+  }
+
   return (
-    <>
-      {!trip ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          <h1>My {trip.title} Trip</h1>
-          <p>Location: {trip.location}</p>
-          <p>Start Date: {trip.start_date}</p>
-          <p>End Date: {trip.end_date}</p>
+    <div>
+      <div className="text-center pb-16">
+        <h1 className="text-5xl font-extrabold">My {trip?.title} Trip</h1>
+        <p className="text-4xl font-extrabold">{trip?.location}</p>
+      </div>
+
+      <div className="flex flex-col justify-between">
+        <div className="py-8">
+          <div>
+            <h2 className="text-2xl font-bold text-center pb-4">Details:</h2>
+          </div>
+
+          <div className="py-2 text-xl">
+            <span className="font-bold pr-4">Where:</span>
+            <span>{trip?.location}</span>
+          </div>
+
+          <div className="py-2 text-xl justify-center">
+            <span className="font-bold pr-4">When:</span>
+            <span>
+              {trip &&
+                `${formatDate(trip.start_date)} - ${formatDate(trip.end_date)}`}
+            </span>
+          </div>
+          <div className="w-full place-items-center justify-center">
+            <button
+              className="px-4 mt-8 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+              onClick={() => router.push(`${trip?.id}/edit`)}
+            >
+              Edit trip details
+            </button>
+          </div>
         </div>
-      )}
-    </>
+
+        <div className="text-2xl text-center font-bold pb-4">
+          <h2>Itenerary</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/app/types.ts
+++ src/app/types.ts
@@ -8,6 +8,7 @@ export type User = {
 export type Trip = {
   id: number
   title: string
+  description: string
   location: string
   start_date: string
   end_date: string
