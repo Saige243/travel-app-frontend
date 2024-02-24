0d99313ce1fe0be37a51e5a924832f947181db92
Saige Cross <saigecrossdev@gmail.com>
2024-02-24 15:45:47 -0600

    auth route working

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,49 @@
-import type { Metadata } from "next"
+"use client"
+
 import "./globals.css"
 import Navbar from "./_components/Navbar"
-
-export const metadata: Metadata = {
-  title: "Travel App",
-  description: "Plan it, baby",
-}
+import { useState, useEffect } from "react"
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  const getUser = async () => {
+    try {
+      const url = `http://localhost:3001/auth/logged_in`
+      const res = await fetch(url, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include", // Necessary for cookies if session-based authentication
+      })
+
+      if (!res.ok) {
+        setIsAuthenticated(false)
+        return
+      }
+
+      const data = await res.json()
+      setIsAuthenticated(data.logged_in)
+      console.log("Logged in status:", data.logged_in)
+    } catch (error) {
+      console.error("Failed to fetch authentication status", error)
+      setIsAuthenticated(false)
+    }
+  }
+
+  useEffect(() => {
+    getUser()
+  }, [])
+
   return (
     <html lang="en">
       <body className="min-h-screen">
-        <Navbar />
+        {isAuthenticated && <Navbar />}
         <main className="lg:px-20 lg:py-12">{children}</main>
       </body>
     </html>
---
+++ src/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Travel App",
+  description: "Plan it, baby",
+}
