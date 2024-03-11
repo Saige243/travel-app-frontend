5bc5075c0b06225b82c742ecbe8a6f684899b834
Saige Cross <saigecrossdev@gmail.com>
2024-03-11 11:53:35 -0500

    added fn/ln to register

--- src/app/_components/SignupForm.tsx
+++ src/app/_components/SignupForm.tsx
@@ -17,6 +17,8 @@ function SignupForm({ onSwitch }: { onSwitch: () => void }) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         user: {
+          first_name: formData.get("first_name"),
+          last_name: formData.get("last_name"),
           email,
           password,
           password_confirmation,
@@ -38,6 +40,38 @@ function SignupForm({ onSwitch }: { onSwitch: () => void }) {
     <div className="p-8 bg-white shadow-md rounded-lg">
       <h2 className="mb-6 text-center text-2xl font-bold">Sign Up</h2>
       <form className="space-y-4" onSubmit={handleSubmit}>
+        <div>
+          <label
+            htmlFor="first_name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            First Name
+          </label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            required
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="first name"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="last_name"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            required
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            placeholder="you@example.com"
+          />
+        </div>
         <div>
           <label
             htmlFor="email"
