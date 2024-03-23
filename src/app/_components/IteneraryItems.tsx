8caa8976e37217eb1b20fef1fccbe21c99827001
Saige Cross <saigecrossdev@gmail.com>
2024-03-22 19:12:38 -0500

    more refactoring

--- src/app/_components/IteneraryItems.tsx
+++ src/app/_components/IteneraryItems.tsx
@@ -48,54 +48,76 @@ const ItineraryItems: React.FC<ItineraryItemsProps> = ({
     setFilteredItems(grouped)
   }, [selectedDate, itineraries])
 
+  const IteneraryRowItem = () => {
+    return Object.entries(filteredItems).map(
+      (
+        [date, items]: [string, ItineraryItem[]],
+        index: React.Key | null | undefined
+      ) => (
+        <div key={index} className="pt-4">
+          <h2 className="font-bold pb-2 text-lg ">{formatDate(date)}</h2>
+          {items.map((item, itemIndex) => (
+            <div
+              key={itemIndex}
+              className="border ml-4 p-3 m-1 rounded shadow space-y-2"
+            >
+              <div className="flex flex-col space-y-3 md:space-y-0 md:grid md:grid-cols-4 justify-between">
+                <div>
+                  <h2 className="font-bold underline">Activity:</h2>
+                  <p>{item.title || "No title provided"}</p>
+                </div>
+                <div>
+                  <h2 className="font-bold underline">Description:</h2>
+                  <p>{item.description || "No description provided"}</p>
+                </div>
+                <div>
+                  <h2 className="font-bold underline">Time:</h2>
+                  <p>
+                    {item.time ? formatTime(item.time) : "No time provided"}
+                  </p>
+                </div>
+                <div>
+                  <h2 className="font-bold underline">Location:</h2>
+                  <p>{item.location || "No location provided"}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )
+    )
+  }
+
+  const categorySelect = (
+    <select
+      value={selectedDate}
+      onChange={(e) => setSelectedDate(e.target.value)}
+      className="p-1 border rounded dark:text-black"
+    >
+      <option>All Dates</option>
+      {[...new Set(itineraries.map((item) => item.date))].map((date) => (
+        <option key={date} value={date}>
+          {formatDate(date)}
+        </option>
+      ))}
+    </select>
+  )
+
+  const addIteneraryItemButton = (
+    <button
+      className="px-4 mt-8 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+      onClick={() => router.push(`${trip?.id}/itinerary/new`)}
+    >
+      Add itinerary items
+    </button>
+  )
+
   return (
     <div>
       {itineraries.length > 0 ? (
         <>
-          <select
-            value={selectedDate}
-            onChange={(e) => setSelectedDate(e.target.value)}
-            className="p-1 border rounded"
-          >
-            <option value="">All Dates</option>
-            {[...new Set(itineraries.map((item) => item.date))].map((date) => (
-              <option key={date} value={date}>
-                {formatDate(date)}
-              </option>
-            ))}
-          </select>
-          {Object.entries(filteredItems).map(([date, items], index) => (
-            <div key={index} className="pt-4">
-              <h2 className="font-bold pb-2 text-lg ">{formatDate(date)}</h2>
-              {items.map((item, itemIndex) => (
-                <div
-                  key={itemIndex}
-                  className="border ml-4 p-3 m-1 rounded shadow space-y-2"
-                >
-                  <div className="flex flex-col space-y-3 md:space-y-0 md:grid md:grid-cols-4 justify-between">
-                    <div>
-                      <h2 className="font-bold underline">Activity:</h2>
-                      <p>{item.title || "No title provided"}</p>
-                    </div>
-                    <div>
-                      <h2 className="font-bold underline">Description:</h2>
-                      <p>{item.description || "No description provided"}</p>
-                    </div>
-                    <div>
-                      <h2 className="font-bold underline">Time:</h2>
-                      <p>
-                        {item.time ? formatTime(item.time) : "No time provided"}
-                      </p>
-                    </div>
-                    <div>
-                      <h2 className="font-bold underline">Location:</h2>
-                      <p>{item.location || "No location provided"}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-          ))}
+          {categorySelect}
+          <IteneraryRowItem />
           <div className="flex space-x-4">
             <div>
               <button
@@ -105,26 +127,14 @@ const ItineraryItems: React.FC<ItineraryItemsProps> = ({
                 Edit itinerary items
               </button>
             </div>
-            <div>
-              <button
-                className="px-4 mt-8 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-                onClick={() => router.push(`${trip?.id}/itinerary/new`)}
-              >
-                Add itinerary items
-              </button>
-            </div>
+            <div>{addIteneraryItemButton}</div>
           </div>
         </>
       ) : (
         <div className="py-6">
           <div className="text-center">
             <p className="text-xl">You have no itinerary items!</p>
-            <button
-              className="px-4 mt-8 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-              onClick={() => router.push(`${trip?.id}/itinerary/new`)}
-            >
-              Add itinerary items
-            </button>
+            {addIteneraryItemButton}
           </div>
         </div>
       )}
